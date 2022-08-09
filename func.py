5bdb5a20bded7a75b7e30e6a41c0072583e4a7c1
Sergio Navarro <sergio.navarro.ricouz@gmail.com>
2022-08-09 09:34:52 -0400

    Agregar excepciones, y comentarios al codigo

--- func.py
+++ func.py
@@ -21,50 +21,52 @@ def obtener_token(): #obtener token
     return token
 
 def Estado_leaf(leaf): #consulta por los dos leaf que estan en el laboratorio
-    TOKEN = obtener_token()
-    url = conf.URL + "node/mo/topology/pod-1/node-"+leaf+"/sys.json?query-target=subtree&target-subtree-class=l1PhysIf,l1FcPhysIf&rsp-subtree-include=health,faults" #url de consulta con la variable que indica al leaf seleccionado
-    cab = {"content-Type": "application/json"}
-    cookies = {"APIC-Cookie": TOKEN} #carga de token como cookie
-    requests.packages.urllib3.disable_warnings()
-    respuesta = requests.get(url, headers=cab, cookies=cookies, verify=False)
-
-    return respuesta
-
+    try:
+        TOKEN = obtener_token()
+        url = conf.URL + "node/mo/topology/pod-1/node-"+leaf+"/sys.json?query-target=subtree&target-subtree-class=l1PhysIf,l1FcPhysIf&rsp-subtree-include=health,faults" #url de consulta con la variable que indica al leaf seleccionado
+        cab = {"content-Type": "application/json"}
+        cookies = {"APIC-Cookie": TOKEN} #carga de token como cookie
+        requests.packages.urllib3.disable_warnings()
+        respuesta = requests.get(url, headers=cab, cookies=cookies, verify=False)
+        return respuesta
+    except:
+        return "Hubo un problema, por favor verifica la conexión y/o los datos ingresados"
 #prueba del puerto 1/49 que esta up
 def adm_puerto(puerto,estado,nodo): #funcion donde tiene las variables del puerto , si quiere encender o apagar la interfaz y si el nodo a modificar
-    TOKEN = obtener_token()
-    if(nodo == '1'):
-        nodo='101'
-    elif(nodo == '2'):
-        nodo = '102'
-    cookies = {"APIC-Cookie": TOKEN}
-    url = conf.URL+"node/mo/uni/fabric/outofsvc.json"
-    if(estado == "2"): #url si se quiere apagar la interfaz
-        body = {
-            "fabricRsOosPath": {
-                "attributes": {
-                    "tDn": "topology/pod-1/paths-"+nodo+"/pathep-[eth"+puerto+"]",
-                    "lc": "blacklist"
-                },
-                "children": []
+    try:
+        TOKEN = obtener_token()
+        if(nodo == '1'):
+            nodo='101'
+        elif(nodo == '2'):
+            nodo = '102'
+        cookies = {"APIC-Cookie": TOKEN}
+        url = conf.URL+"node/mo/uni/fabric/outofsvc.json"
+        if(estado == "2"): #url si se quiere apagar la interfaz
+            body = {
+                "fabricRsOosPath": {
+                    "attributes": {
+                        "tDn": "topology/pod-1/paths-"+nodo+"/pathep-[eth"+puerto+"]",
+                        "lc": "blacklist"
+                    },
+                    "children": []
+                }
             }
-        }
-    elif(estado == "1"):#url si se quiere encernder la interfaz
-        body = {
-            "fabricRsOosPath": {
-                "attributes": {
-                    "dn": "uni/fabric/outofsvc/rsoosPath-[topology/pod-1/paths-"+nodo+"/pathep-[eth"+puerto+"]]",
-                    "status": "deleted"
-                },
-                "children": []
+        elif(estado == "1"):#url si se quiere encernder la interfaz
+            body = {
+                "fabricRsOosPath": {
+                    "attributes": {
+                        "dn": "uni/fabric/outofsvc/rsoosPath-[topology/pod-1/paths-"+nodo+"/pathep-[eth"+puerto+"]]",
+                        "status": "deleted"
+                    },
+                    "children": []
+                }
             }
+        cabecera = {
+            "Content-Type": "application/json"
         }
-
-    cabecera = {
-        "Content-Type": "application/json"
-    }
-    requests.packages.urllib3.disable_warnings()
-    respuesta = requests.post(url, headers=cabecera, cookies=cookies, data=json.dumps(body), verify=False)
-    token = respuesta #respuesta si fue o no ejecutada
-    return token
-
+        requests.packages.urllib3.disable_warnings()
+        respuesta = requests.post(url, headers=cabecera, cookies=cookies, data=json.dumps(body), verify=False)
+        token = respuesta #respuesta si fue o no ejecutada
+        return token
+    except:#para controlar algun error devolviendo un estring
+        return "Hubo un problema, por favor verifica la conexión y/o los datos ingresados"
--- main.py
+++ main.py
@@ -4,17 +4,18 @@ print("Ingrese valor (1,2,3) ", end="")  # espera de ingreso
 opcion = input()
 #prueba del puerto 1/49 que esta up
 if opcion == '1':
-    estado = func.Estado_leaf('101')
-    total = int(estado.json()['totalCount'])
-    print(' Total interfases :',estado.json()['totalCount'], '\n','ID  ,  Estado administrativo,  Modo , Auto Negociacion')
+    estado = func.Estado_leaf('101') # llamo funcion del leaf 1 que tiene codigo 101
+    total = int(estado.json()['totalCount']) #saco la cantidad de puertos que tiene el leaf
+    print(' Total interfases :',estado.json()['totalCount'], '\n','ID  ,  Estado administrativo,  Modo , Auto Negociacion') # Imprimo la cabecera de la lista
     x = 0
+    # while para recorer todo el json sacando datos nececitados
     while x < total:
         print(estado.json()['imdata'][x]['l1PhysIf']['attributes']['id'], '          ', estado.json()['imdata'][x]['l1PhysIf']['attributes']['adminSt'], '       ',estado.json()['imdata'][x]['l1PhysIf']['attributes']['mode'], '    ', estado.json()['imdata'][x]['l1PhysIf']['attributes']['autoNeg'])
         x += 1
 if opcion == '2':
-    estado = func.Estado_leaf('102')
-    total = int(estado.json()['totalCount'])
-    print(' Total interfases :',estado.json()['totalCount'], '\n','ID  ,    Estado administrativo,  Modo , Auto Negociacion')
+    estado = func.Estado_leaf('102') # llamo funcion del leaf 2 que tiene codigo 102
+    total = int(estado.json()['totalCount'])#saco la cantidad de puertos que tiene el leaf
+    print(' Total interfases :', estado.json()['totalCount'], '\n', 'ID  ,    Estado administrativo,  Modo , Auto Negociacion')
     x = 0
     while x < total:
         print(estado.json()['imdata'][x]['l1PhysIf']['attributes']['id'], '       ', estado.json()['imdata'][x]['l1PhysIf']['attributes']['adminSt'], '          ',estado.json()['imdata'][x]['l1PhysIf']['attributes']['mode'], '      ', estado.json()['imdata'][x]['l1PhysIf']['attributes']['autoNeg'])
@@ -23,8 +24,15 @@ if opcion == '3':
     print("Que leaf quiere modificar 1 o 2? ", end="")
     leaf = input()
     print("Que interfas quiere modificar formato X/XX? ", end="")
-    interfaz= input()
+    interfaz = input()
     print("1 para subir 2 para bajar administrativamente ", end="")
     updown = input()
-    est = func.adm_puerto(interfaz,updown,leaf)
+    est = func.adm_puerto(interfaz,updown,leaf) #envio datos para realizar la modificacion
     print(est)
+    try:#utilizo esta funcion para poder determinar cuando se ingrese datos incorrectos en esta seccion
+        if est.status_code == 200: #espero el codigo 200 si el cambio fuen realizado correctamente
+            print("Cambios realizados correctamente, en unos segundos se reflejara en el controlador.")
+    except:
+        print()
+else:
+    print("Por favor elija una opción valida") # si se ingresa un dato incorrecto en el menu principal
